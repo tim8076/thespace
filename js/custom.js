51149b2411c3fb23cf9692fd4ef84cc9bad50f08
tim8076 <69451071+tim8076@users.noreply.github.com>
2020-08-13 23:41:42 +0800

    0813
    0813

---
+++ js/custom.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+        //禮品店側邊選單收合
+   $(".list-toggle").click(function(){
+    $(".slide").slideToggle(200);
+   })
+
+       //首頁選單收合
+    $(".navbar__info > li").hover(function(){
+      $(this).children(".dropdown").stop().slideToggle(200);
+    })
+   
+      //首頁頁籤
+    $("#contents div[id != 'content1']").hide();
+
+    $(".service-list-item").click(function(){
+      $("#contents .content-item").hide();
+      $($(this).attr("href")).show();
+
+      $(".current").removeClass("current");
+      $(this).addClass("current");
+
+      return false;
+    })
+
+    //聯絡我們-平滑捲動
+    $("a[href='#contactus']").click(function(){
+
+      var target = $($(this).attr("href")).offset().top;
+
+      $("html,body").animate({scrollTop:target},500);
+
+
+      return false;
+    })   
+  
+});
+
+
+
+   
+
